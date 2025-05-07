1e54e3cfe98c1359d5385342649c1272ae9af3e8
Muhammad Chaerullah <chaerullahmuhammad@gmail.com>
2025-05-07 19:32:53 +0700

    My portfolio : Three Js

--- app.vue
+++ app.vue
@@ -11,50 +11,61 @@
       }"
     >
     </div>
-    <div class="mx-auto max-w-6xl sm:p-20 px-5 py-10">
-      <div class="flex flex-col gap-20 sm:flex-row">
-        <div class="flex flex-col gap-8 sm:gap-96">
+    <div class="mx-auto md:max-w-10/12 md:p-20 px-5 py-10">
+      <div class="flex flex-col gap-20 md:flex-row">
+        <div class="flex flex-col gap-8 md:gap-96 md:w-6/12">
           <div class="flex flex-col gap-5">
             <div class="flex flex-col">
-              <h1 class="text-3xl font-bold tracking-tight text-slate-200 sm:text-4xl font-poppins">
+              <h1 class="text-3xl font-bold tracking-tight text-slate-200 md:text-5xl font-poppins">
                 Muhammad Chaerullah
               </h1>
-              <p class="text-2xl font-light tracking-tight text-slate-200 sm:text-3xl">
+              <p class="text-2xl font-light tracking-tight text-slate-200 md:text-3xl">
                 Front End Engineer
               </p>
             </div>
             <div>
-              <p class="text-lg font-light tracking-tight text-slate-400 sm:text-xl">
-                Designing seamless digital experiences — accessible, beautiful, and fast.
+              <p class="text-lg font-light tracking-tight text-slate-400 md:text-xl">
+                Designing seamless digital experiences <br> accessible, beautiful, and fast.
               </p>
             </div>
           </div>
           <div class="flex gap-5">
-            <Icon name="uil:github" class="text-slate-400 hover:text-slate-100 sm:text-4xl text-3xl cursor-pointer" />
-            <Icon name="uil:linkedin" class="text-slate-400 hover:text-slate-100 sm:text-4xl text-3xl cursor-pointer" />
-            <Icon name="uil:instagram" class="text-slate-400 hover:text-slate-100 sm:text-4xl text-3xl cursor-pointer" />
+            <Icon name="uil:github" class="text-slate-400 hover:text-slate-100 md:text-4xl text-3xl cursor-pointer" />
+            <Icon name="uil:linkedin" class="text-slate-400 hover:text-slate-100 md:text-4xl text-3xl cursor-pointer" />
+            <Icon name="uil:instagram" class="text-slate-400 hover:text-slate-100 md:text-4xl text-3xl cursor-pointer" />
           </div>
         </div>
-        <div class="w-3/4">
-          I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.
-
-          Currently, I'm a Senior Front-End Engineer at Klaviyo, specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.
-
-          In the past, I've had the opportunity to develop software across a variety of settings — from advertising agencies and large corporations to start-ups and small digital product studios. Additionally, I also released a comprehensive video course a few years ago, guiding learners through building a web app with the Spotify API.
-
-          In my spare time, I’m usually climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds
-          K
-          o
-          r
-          o
-          k
-          
-          s
-          e
-          e
-          d
-          s
-          .
+        <div class="md:w-6/12">
+          <div class="flex flex-col gap-10">
+            <div>
+              <p class="text-justify">
+                <span class="font-poppins font-bold">Front End Developer</span> with over 3 years of experience specializing in building responsive, user-centric
+                web applications using Vue.js, Nuxt.js, and Tailwind CSS. Skilled at translating UI/UX designs into
+                high-quality, maintainable code. Passionate about optimizing user experience, integrating APIs, and
+                collaborating with cross-functional teams to deliver impactful web solutions.
+              </p>
+            </div>
+            <div class="flex flex-wrap justify-between">
+              <div>
+                <ThreeViewer/>
+              </div>
+              <div>
+                <ThreeViewer/>
+              </div>
+              <div>
+                <ThreeViewer/>
+              </div>
+              <div>
+                <ThreeViewer/>
+              </div>
+              <div>
+                <ThreeViewer/>
+              </div>
+              <div>
+                <ThreeViewer/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -63,6 +74,7 @@
 
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue'
+import ThreeViewer from '~/components/ThreeViewer.vue'
 
 const position = ref({ x: 0, y: 0 })
 
